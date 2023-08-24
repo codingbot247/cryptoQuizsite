37a922b8239415e78df5c11c0931d1809845c468
codingbot <58110004+codingbot247@users.noreply.github.com>
2023-08-24 22:43:48 +0530

    Update connectWallet.js

--- connectWallet.js
+++ connectWallet.js
@@ -7,7 +7,7 @@ const provider = new ethers.providers.Web3Provider(window.ethereum, "goerli");
     provider.send("eth_requestAccounts", []).then(() => {
     provider.listAccounts().then(function(accounts) {
       signer = provider.getSigner(accounts[0]);
-      MoodContract = new ethers.Contract(MoodContractAddress, MoodContractABI, signer);
+      QuizContract = new ethers.Contract(QuizContractAddress, QuizContractABI, signer);
     });
 
   });
