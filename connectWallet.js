5106ee5108c50f82fa74d4dc9868bea58b4b4194
codingbot <58110004+codingbot247@users.noreply.github.com>
2023-09-19 23:59:03 +0530

    Update connectWallet.js

--- connectWallet.js
+++ connectWallet.js
@@ -1,11 +1,18 @@
+  let userAddress=null;
+
 async function connectWallet(){
     if (typeof window.ethereum !== "undefined"){
-        await window.ethereum.request({method: "eth_requestAccounts"})
-        document.getElementbyId("connect").innerHTML = "Connected"
+         /*await window.ethereum.request({
+            method: 'wallet_switchEthereumChain',
+            params: [{ chainId: '0x84531' ,
+                       rpcURL: 'https://chain-proxy.wallet.coinbase.com?targetName=base-goerli' }], 
+          });*/
+       userAddress = await window.ethereum.request({method: "eth_requestAccounts"})
+       
+        document.getElementById("connect").innerHTML = "Connected";
+        console.log(userAddress);
     }
     else{
-        document.getElementById("connect").innerHTML = "Please install a crypto wallet"
+        document.getElementById("connect").innerHTML = "Please install a crypto wallet" 
     }
 }
-
-
